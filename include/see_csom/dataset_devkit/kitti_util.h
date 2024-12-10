33891f99b47a0cae101de6977c54187f2b9edf8e
dzp62442 <dzp62442@163.com>
2024-12-10 16:48:01 +0800

    WSL本地编译运行成功，但rviz可视化失败

--- include/see_csom/dataset_devkit/kitti_util.h
+++ include/see_csom/dataset_devkit/kitti_util.h
@@ -3,7 +3,8 @@
 #include <fstream>
 
 #include <Eigen/Dense>
-#include <opencv/cv.hpp>
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgproc.hpp>
 #include <opencv2/core/core.hpp>
 #include <pcl/common/transforms.h>
 
